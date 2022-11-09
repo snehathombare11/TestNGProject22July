b236474b48073e023e2d022e726ba1d3c6556f9d
snehathombare11 <snehathombare11@gmail.com>
2022-11-09 09:17:49 +0530

    added file

--- src/test/java/TestCases/VerifyLogin.java
+++ src/test/java/TestCases/VerifyLogin.java
@@ -1,18 +1,28 @@
 package TestCases;
 
 import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import PageObjectModel.LoginPageObjects;
+import Resources.CommonUtilities;
 import Resources.baseClass;
+import Resources.constant;
 
 public class VerifyLogin extends baseClass{
 	
 	   @Test
 	   public void login() throws IOException {
-		initializeDriver();
-		driver.get("https://login.salesforce.com/?locale=in");
-	
-	
-	
+		
+		LoginPageObjects obj=new LoginPageObjects(driver);
+		obj.enterUsername().sendKeys(constant.username);
+		obj.enterPassword().sendKeys(constant.Password);
+		obj.clickOnLogin().click();
+		
+   CommonUtilities.Handleassertion(constant.expectedString,obj.actualErrorMassage().getText());
+		
+	   }
 	}	
 	
-}
